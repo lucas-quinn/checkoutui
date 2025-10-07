4ff0d6dc22be33a110856b3a4e706afdf2c5542d
Lucas Quinn <lucas.quinn.tech@gmail.com>
2025-10-07 15:28:55 +1300

    fix

--- extensions/checkout-ui/shopify.d.ts
+++ extensions/checkout-ui/shopify.d.ts
@@ -1,7 +1,13 @@
 import '@shopify/ui-extensions';
 
 //@ts-ignore
-declare module './src/Checkout.jsx' {
+declare module './src/Banner.jsx' {
   const shopify: import('@shopify/ui-extensions/purchase.checkout.block.render').Api;
+  const globalThis: { shopify: typeof shopify };
+}
+
+//@ts-ignore
+declare module './src/Footer.jsx' {
+  const shopify: import('@shopify/ui-extensions/purchase.checkout.footer.render-after').Api;
   const globalThis: { shopify: typeof shopify };
 }
---
+++ extensions/checkout-ui/src/Footer.jsx
@@ -0,0 +1,54 @@
+import "@shopify/ui-extensions/preact";
+import { render } from "preact";
+
+// 1. Export the extension
+export default async () => {
+  render(<Extension />, document.body);
+};
+
+function Extension() {
+  const features = [
+    {
+      icon: "https://cdn.shopify.com/s/files/1/0688/5235/1153/files/money.svg",
+      title: "30-Day Money-Back Guarantee",
+      description:
+        "Shop confidently. Quick and simple refunds, no questions asked.",
+    },
+    {
+      icon: "https://cdn.shopify.com/s/files/1/0688/5235/1153/files/secure.svg",
+      title: "Secure Checkout",
+      description:
+        "In partnership with Chase Bank, your payments are processed safely and securely.",
+    },
+    {
+      icon: "https://cdn.shopify.com/s/files/1/0688/5235/1153/files/tracking.svg",
+      title: "Real-Time Tracking",
+      description:
+        "Real-time updates at every stage of delivery — plus a 24/7 customer support team to help anytime.",
+    },
+  ];
+
+  return (
+    <s-grid gridTemplateColumns="1fr" gap="large" padding="base">
+      {features.map((feature) => (
+        <s-grid
+          key={feature.title}
+          gridTemplateColumns="auto 1fr"
+          gap="base"
+          alignItems="start"
+        >
+          <s-image
+            src={feature.icon}
+            alt={feature.title}
+            inlineSize="auto"
+            aspectRatio="1/1"
+          ></s-image>
+          <s-grid gridTemplateColumns="1fr" gap="large">
+            <s-heading>{feature.title}</s-heading>
+            <s-text>{feature.description}</s-text>
+          </s-grid>
+        </s-grid>
+      ))}
+    </s-grid>
+  );
+}
